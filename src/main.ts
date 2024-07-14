561a8e4d2889ca84e6cf9365cb1fc070cdfa24c0
Nick Rabone <n.rabone72@aol.com>
2024-07-14 15:35:21 +0100

    Add Stats panel

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+import Stats from 'three/addons/libs/stats.module.js'
 
 const scene = new THREE.Scene()
 
@@ -25,13 +26,18 @@ const material = new THREE.MeshNormalMaterial({ wireframe: true })
 const cube = new THREE.Mesh(geometry, material)
 scene.add(cube)
 
+const stats = new Stats()
+document.body.appendChild(stats.dom)
+
 function animate() {
   requestAnimationFrame(animate)
 
-  cube.rotation.x += 0.01
-  cube.rotation.y += 0.01
+  // cube.rotation.x += 0.01
+  // cube.rotation.y += 0.01
 
   renderer.render(scene, camera)
+
+  stats.update()
 }
 
 animate()
